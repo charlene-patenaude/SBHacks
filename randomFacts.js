c274511de599409061cbd723f922b3ff163efd6c
charlene-patenaude <97643638+charlene-patenaude@users.noreply.github.com>
2024-01-13 14:01:44 -0800

    Update randomFacts.js
    added newFact func and randNum

--- randomFacts.js
+++ randomFacts.js
@@ -3,3 +3,8 @@ var facts = [
   'quote2'
   ] 
   
+function newFact() {
+  var randNum = Math.floor(Math.random() * facts.length));
+  document.getElementById('factDisplay').innerHTML = facts[randNum];
+
+}
