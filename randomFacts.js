5ab498ad976c580712d279dd3a62cb3cc4582d3e
psabherwal <97641119+psabherwal@users.noreply.github.com>
2024-01-13 22:03:46 -0800

    Update randomFacts.js

--- randomFacts.js
+++ randomFacts.js
@@ -5,6 +5,6 @@ var facts = [
   
 function newFact() {
   var randNum = Math.floor(Math.random() * facts.length);
-  document.getElementById('factDisplay').innerHTML = facts[randNum];
 
 }
+document.getElementById('factDisplay').innerHTML = facts[randNum];
